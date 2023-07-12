80ca623e3243026a5b75d2ea50b4279201fba1c3
Nat Welch <nat@natwelch.com>
2023-07-12 00:39:49 +0000

    shift

--- app.go
+++ app.go
@@ -50,8 +50,8 @@ func getCurrentBrightness(latitude, longitude float64) (brightness float64, err 
 	// Calculate the brightness using a sine function
 	brightness = math.Sin(2 * math.Pi * hours / 24)
 
-	// Adjust the brightness range from [-1, 1] to [0, 1]
-	brightness = (brightness + 1) / 2
+	// Adjust the brightness range from [-1, 1] to [0, 0.5]
+	brightness = (brightness + 1) / 4
 
 	return brightness, nil
 }
