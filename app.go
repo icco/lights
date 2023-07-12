f60a72d3130d24151278cb49a0423c1e1184f644
Nat Welch <nat@natwelch.com>
2023-07-12 01:27:49 +0000

    revert

--- app.go
+++ app.go
@@ -26,8 +26,6 @@ func getTwilightTimes() (time.Time, time.Time, error) {
 	end := times[suncalc.Sunset].Value.Local()
 	start := times[suncalc.Sunrise].Value.Local()
 
-	log.Printf("twilight: %s -> %s", start, end)
-
 	return start, end, nil
 }
 
@@ -51,10 +49,7 @@ func getCurrentBrightness() (float64, error) {
 	log.Printf("now: %s -> (middle: %s) %s", now, equidistant, duration)
 
 	// Calculate the brightness using a sine function
-	brightness := math.Sin(2 * math.Pi * sec / 86400)
-
-	// Adjust the brightness range from [-1, 1] to [0, 0.5]
-	brightness = (brightness + 1) / 4
+	brightness := math.Sin(2 * math.Pi * (sec / 86400))
 
 	return brightness, nil
 }
