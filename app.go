81f70476c66140bac6552c3410992ae614ae93fb
Nat Welch <nat@natwelch.com>
2023-07-12 01:20:11 +0000

    shrink

--- app.go
+++ app.go
@@ -47,10 +47,10 @@ func getCurrentBrightness() (float64, error) {
 	duration := now.Sub(equidistant)
 
 	// Convert the duration to hours
-	hours := duration.Hours()
+	sec := duration.Seconds()
 
 	// Calculate the brightness using a sine function
-	brightness := math.Sin(2 * math.Pi * hours / 24)
+	brightness := math.Sin(2 * math.Pi * sec / 24)
 
 	// Adjust the brightness range from [-1, 1] to [0, 0.5]
 	brightness = (brightness + 1) / 4
